f1d61efbd052986d47d4e5221bec7ee9f8748309
Busisiwe <91269263+Busi23@users.noreply.github.com>
2025-07-23 19:46:32 +0200

    Update Netflix.py

--- Netflix.py
+++ Netflix.py
@@ -206,4 +206,4 @@ def update_graphs(selected_year, selected_type):
 # Run the app
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8050))
-    app.run_server(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
