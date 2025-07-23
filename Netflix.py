a8521d2b2dda74fb61d1ac98c1947b214ce65249
Busisiwe <91269263+Busi23@users.noreply.github.com>
2025-07-23 19:27:02 +0200

    Update Netflix.py

--- Netflix.py
+++ Netflix.py
@@ -5,7 +5,7 @@ import plotly.express as px
 import pandas as pd 
 
 # Loading the Netflix dataset
-netflix = pd.read_csv("C:/Users/b/OneDrive/Desktop/School/Data manipulation/netflix_titles.csv")
+netflix = pd.read_csv("https://raw.githubusercontent.com/Busi23/Netflix--dashboard/refs/heads/main/netflix_titles.csv")
 
 # Extracting the years for dropdown filter 
 year_options = [{'label': str(year), 'value': year} for year in sorted(netflix['release_year'].dropna().unique())]
